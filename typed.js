230961d80799a1c5a12b1ff76e3298a7e132c5fd
Marta Kule <marta.kule@gmail.com>
2017-05-01 13:14:56 -0400

    changed cursor character from block to triangle

--- typed.js
+++ typed.js
@@ -479,7 +479,7 @@
 		// show cursor
 		showCursor: true,
 		// character for cursor
-		cursorChar: " &#9612;",
+		cursorChar: " &#9654;",
 		// attribute to type (null == text)
 		attr: null,
 		// either html or text
